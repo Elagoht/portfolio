e1d929006f34c955c6bc3f2ef57c2da959525946
Furkan Baytekin <furkanbaytekin@gmail.com>
2026-02-15 08:45:50 +0300

    feat: new footer

--- internal/handlers/shared.go
+++ internal/handlers/shared.go
@@ -5,6 +5,11 @@ type Link struct {
 	Href  string
 }
 
+type LinkGroup struct {
+	Label string
+	Links []Link
+}
+
 var SiteName = "Furkan Baytekin"
 var SiteEmail = "furkan@baytekin.dev"
 
@@ -20,11 +25,39 @@ var SiteLinks = []Link{
 	{Title: "Itch.io", Href: "https://elagoht.itch.io"},
 }
 
+var FooterGroups = []LinkGroup{
+	{
+		Label: "Social",
+		Links: []Link{
+			{Title: "GitHub", Href: "https://github.com/Elagoht"},
+			{Title: "LinkedIn", Href: "https://linkedin.com/in/furkan-baytekin"},
+			{Title: "X", Href: "https://x.com/furkanbytekin"},
+			{Title: "Reddit", Href: "https://reddit.com/u/furkanbytekin"},
+		},
+	},
+	{
+		Label: "Content",
+		Links: []Link{
+			{Title: "YouTube", Href: "https://youtube.com/@furkanbytekin"},
+			{Title: "Udemy", Href: "https://www.udemy.com/user/furkan-baytekin/"},
+			{Title: "Itch.io", Href: "https://elagoht.itch.io"},
+		},
+	},
+	{
+		Label: "Other",
+		Links: []Link{
+			{Title: "Telegram", Href: "https://t.me/furkanbytekin"},
+			{Title: "Spotify", Href: "https://open.spotify.com/user/furkanbytekin"},
+		},
+	},
+}
+
 func BaseData(lang string) map[string]any {
 	return map[string]any{
-		"Lang":  lang,
-		"Name":  SiteName,
-		"Email": SiteEmail,
-		"Links": SiteLinks,
+		"Lang":         lang,
+		"Name":         SiteName,
+		"Email":        SiteEmail,
+		"Links":        SiteLinks,
+		"FooterGroups": FooterGroups,
 	}
 }
